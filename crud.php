b001e462ca8d28169ba27e99e9b074c5938fa102
Doskhan <doskhan.doszhanov@is.sdu.edu.kz>
2018-07-17 20:46:19 +0600

    some changes

--- crud.php
+++ crud.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Registration</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<script src="js/bootstrap.min.js"></script> 
-</head>
-<body>
-	<div class="container">
-	<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.php">Home</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="login.php">Login </a></li>
-        <li><a href="register.php">Register </a></li>
-        <li class="active"><a href="crud.php">CRUD </a></li>
-        
-      </ul>
-      
-      <ul class="nav navbar-nav navbar-right">
-        <li><a>Username</a></li>
-        <li><a href="#">Logout</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
+<?php
+  include 'header.php';
+?>
 <div class="table-responsive">
   <table class="table">
     <tr>
@@ -57,13 +21,19 @@
 
       <td><button class="btn btn-danger" type="submit">Delete</button></td>
     </tr>
-    <tr><th><button class="btn btn-success" type="submit">Add</button></th></tr>
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th></th>
+      <th><button class="btn btn-success" type="submit">Add</button></th></tr>
   </table>
 </div>
 
 
  
 </div>
-</div>
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,42 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Registration</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<script src="js/bootstrap.min.js"></script> 
-</head>
-<body>
-	<div class="container">
-	<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.php">Home</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="login.php">Login </a></li>
-        <li><a href="register.php">Register </a></li>
-        <li><a href="crud.php">CRUD </a></li>
-      </ul>
-
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-
+<?php include 'header.php'; ?>
 <div class="row">
 	<div class="col-sm-2"></div>
 	<div class="col-sm-8">
-		<form action="/action_page.php">
+		<form action="/action_page.php" method="POST">
   <div class="form-group">
     <label for="email">Email address:</label>
     <input type="email" class="form-control" id="email">
